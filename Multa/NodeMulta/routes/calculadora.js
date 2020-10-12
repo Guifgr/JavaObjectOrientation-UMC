a96599e8779aeb50c0a79981d3b90c8efb182f49
guifgr <guifgr@hotmail.com>
2020-10-12 11:33:05 -0300

    all errors fix

--- Multa/NodeMulta/routes/calculadora.js
+++ Multa/NodeMulta/routes/calculadora.js
@@ -6,12 +6,14 @@ router.post('/', (req, res) =>{
     const dados = req.body
     multa.setVelocidadeAtual(dados.velocidade)
     multa.setVelocidadeLimite(dados.limite)
-    multa.calcMultaPontos()
-    multa.calcMultaValor()
     var Mensagem;
     if(isNaN(dados.velocidade) || isNaN(dados.limite)){
-        Mensagem='Algo deu errado ao passar os dados!'
+        Mensagem='Algum dos dados vieram como simbolos que não eram numeros!'
+    }else if(dados.velocidade === '' || dados.velocidade === null || dados.limite === '' || dados.limite === null){
+        Mensagem='Algum dos dados estavam vazios!!!'
     }else{
+        multa.calcMultaPontos()
+        multa.calcMultaValor()
         Mensagem = `A multa atual é ${multa.getMultaValor()} e os pontos são ${multa.getMultaPontos()}`;
     }
     res.render('resultado', { Mensagem })
